227a5832cdb15c75aa014015f33d87fefeebc8d7
kaythi <kaythi.bob@icloud.com>
2022-09-27 17:26:02 -0400

    first commit

---
+++ week1/test.js
@@ -0,0 +1,53 @@
+console.log("hello world");
+console.log(2);
+console.log(2+2);
+console.log(10/3);
+console.log(true || false);
+console.log(true || true);
+console.log(false || true);
+console.log(false || false);
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+let str = "hello";
+console.log(str[1]);
+console.log(str.length);
+console.log("hello".length);
+console.log("hello".indexOf('h'));
+console.log("hello".indexOf('l'));
+console.log("hello".indexOf('x'));
+console.log("hello".indexOf('hell'));
+console.log("race".concat("car"));
+console.log("race"+"car");
+console.log("hello"+" "+"world");
+console.log(4+"Hi");
+console.log(4*"Hi");
+console.log(4*"10");
+console.log("Hello".toUpperCase());
+console.log("HELLO".toLowerCase());
+console.log("hello".slice());
+console.log("hello".slice(1));
+console.log("hello".slice(1,3));
+let name1 = "Jonathan";
+console.log(name1);
+console.log(name1==="Jonathan");
+let test
+console.log(test);
+if(20===20)
+{
+    console.log("they are equal");
+}
+else
+{
+    console.log("not equal");
+}
+let num = 20;
+if(num === 20){
+    console.log("equals 20");
+} else if (num > 20){
+    console.log("greater than 20");
+} else { 
+    console.log("less than");
+}
+
